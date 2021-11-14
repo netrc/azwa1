aa48028c0920f8c6c0db52d6003487275ed47bd6
Richard Campbell <ric@netrc.com>
2021-11-14 12:34:48 -0500

    header and user version zero

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 
 
-const main = () => {
-  const el = document.getElementById("hw")
-  el.innerHTML = 'from index.js'
+let user = {
+  role: 'unauthenticated',
+  name: 'none',
+  email: 'n@none'
 }
 
+const getUser = async () => {
+  const response = await fetch("/.auth/me").catch( err => {
+    console.log('caught error')
+  })
+  const payload = await response.json()
+  const { clientPrincipal } = payload
+
+  this.user = clientPrincipal
+}
+
+const envVal = (s) => ((typeof(process)!='undefined') && process.env[s] || `env[${s}] not defined`)
+
+const main = async () => {
+  const el = document.getElementById("hw")
+  el.innerHTML = 'from index.js'
+
+  console.log(`checking env: ${envVal('AZWA1_SOMETHING')}`)
+
+  getUser()
+  
+}
 
 main()
